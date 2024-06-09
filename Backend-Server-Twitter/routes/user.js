b800d868b787e45c23b7839b38b4a86c3e91351b
Nishant Maheshwari <mnishant222@gmail.com>
2024-06-09 20:45:23 +0530

    implemented entity routes

--- Backend-Server-Twitter/routes/user.js
+++ Backend-Server-Twitter/routes/user.js
@@ -1,9 +1,12 @@
-const express = require('express');
-const { followUser, unfollowUser } = require('../controllers/userController');
-const authMiddleware = require('../middlewares/authMiddleware');
+import express from "express";
+import { fetchSuggestedEntities, fetchUserDetails, modifyEntityDetails, modifyFollowStatus } from "../controllers/user.controller.js";
+import { authSecurity } from "../middleware/authSecurity.js";
+
 const router = express.Router();
 
-router.post('/follow/:id', authMiddleware, followUser);
-router.post('/unfollow/:id', authMiddleware, unfollowUser);
+router.get("/profile/:username", authSecurity, fetchUserDetails);
+router.get("/suggested", authSecurity, fetchSuggestedEntities);
+router.post("/follow/:id", authSecurity, modifyFollowStatus);
+router.post("/update", authSecurity, modifyEntityDetails);
 
-module.exports = router;
+export default router;
