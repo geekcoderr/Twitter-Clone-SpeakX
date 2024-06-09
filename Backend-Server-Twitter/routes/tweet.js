d9cf8ed33734dc971ca12248ad11d6c3344b5621
Nishant Maheshwari <mnishant222@gmail.com>
2024-06-09 20:44:41 +0530

    added tweet routes to operation

--- Backend-Server-Twitter/routes/tweet.js
+++ Backend-Server-Twitter/routes/tweet.js
@@ -1,9 +1,25 @@
-const express = require('express');
-const { createTweet, getTweets } = require('../controllers/tweetController');
-const authMiddleware = require('../middlewares/authMiddleware');
+import express from "express";
+import {
+    addComment,
+    createTweet,
+    getAllTweets,
+    getFollowingTweets,
+    getLikedTweets,
+    getUserTweets,
+    removeTweet,
+    toggleLikeTweet
+} from "../controllers/post.controller.js";
+import { authSecurity } from "../middleware/authSecurity.js";
+
 const router = express.Router();
 
-router.post('/', authMiddleware, createTweet);
-router.get('/', authMiddleware, getTweets);
+router.get("/all", authSecurity, getAllTweets); 
+router.get("/following", authSecurity, getFollowingTweets); 
+router.get("/likes/:id", authSecurity, getLikedTweets); 
+router.get("/user/:username", authSecurity, getUserTweets); 
+router.post("/create", authSecurity, createTweet); 
+router.post("/like/:id", authSecurity, toggleLikeTweet); 
+router.post("/comment/:id", authSecurity, addComment); 
+router.delete("/:id", authSecurity, removeTweet); 
 
-module.exports = router;
+export default router;
