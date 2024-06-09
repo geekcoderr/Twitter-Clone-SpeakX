5d36ce0e804a746375b3aaf1db344199c9af1942
Nishant Maheshwari <mnishant222@gmail.com>
2024-06-10 02:35:51 +0530

    modified server.js module

--- Backend-Server-Twitter/server.js
+++ Backend-Server-Twitter/server.js
@@ -2,7 +2,6 @@ import { v2 as cloudinary } from "cloudinary";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import express from "express";
-import path from "path";
 
 import authRoutes from "./routes/auth.js";
 import postRoutes from "./routes/tweet.js";
@@ -20,25 +19,15 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
 
-app.use(express.json({ limit: "4mb" })); // to parse req.body
-// limit shouldn't be too high to prevent DOS
-app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
+app.use(express.json({ limit: "4mb" })); 
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
-
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
-}
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
