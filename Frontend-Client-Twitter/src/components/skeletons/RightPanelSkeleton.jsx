8c5a2ecf1a79808ae74bbd4256edbb77cd58cd3f
Nishant Maheshwari <mnishant222@gmail.com>
2024-06-10 21:14:27 +0530

    some features refined

--- Frontend-Client-Twitter/src/components/skeletons/RightPanelSkeleton.jsx
+++ Frontend-Client-Twitter/src/components/skeletons/RightPanelSkeleton.jsx
@@ -1,17 +1,17 @@
 const RightPanelSkeleton = () => {
-	return (
-		<div className='flex flex-col gap-2 w-52 my-2'>
-			<div className='flex gap-2 items-center'>
-				<div className='skeleton w-8 h-8 rounded-full shrink-0'></div>
-				<div className='flex flex-1 justify-between'>
-					<div className='flex flex-col gap-1'>
-						<div className='skeleton h-2 w-12 rounded-full'></div>
-						<div className='skeleton h-2 w-16 rounded-full'></div>
-					</div>
-					<div className='skeleton h-6 w-14 rounded-full'></div>
-				</div>
-			</div>
-		</div>
-	);
+    return (
+        <div className='flex flex-col gap-2 w-52 my-2'> {/* Container for right panel skeleton */}
+            <div className='flex gap-2 items-center'> {/* Flex container for user information */}
+                <div className='skeleton w-8 h-8 rounded-full shrink-0'></div> {/* Skeleton for user avatar */}
+                <div className='flex flex-1 justify-between'> {/* Flex container for user details */}
+                    <div className='flex flex-col gap-1'> {/* Container for user details */}
+                        <div className='skeleton h-2 w-12 rounded-full'></div> {/* Skeleton for username */}
+                        <div className='skeleton h-2 w-16 rounded-full'></div> {/* Skeleton for user status */}
+                    </div>
+                    <div className='skeleton h-6 w-14 rounded-full'></div> {/* Skeleton for follow button */}
+                </div>
+            </div>
+        </div>
+    );
 };
-export default RightPanelSkeleton;
+export default RightPanelSkeleton; // Export RightPanelSkeleton component
--- Frontend-Client-Twitter/src/pages/home/HomePage.jsx
+++ Frontend-Client-Twitter/src/pages/home/HomePage.jsx
@@ -1,45 +1,46 @@
-import { useState } from "react";
+import { useState } from "react"; // Import useState from React
 
-import Posts from "../../components/common/Posts";
-import CreatePost from "./CreatePost";
+import Posts from "../../components/common/Posts"; // Import Posts component
+import CreatePost from "./CreatePost"; // Import CreatePost component
 
 const HomePage = () => {
-	const [feedType, setFeedType] = useState("forYou");
+    const [feedType, setFeedType] = useState("forYou"); // State for feed type
 
-	return (
-		<>
-			<div className='flex-[4_4_0] mr-auto border-r border-gray-700 min-h-screen'>
-				{/* Header */}
-				<div className='flex w-full border-b border-gray-700'>
-					<div
-						className={
-							"flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative"
-						}
-						onClick={() => setFeedType("forYou")}
-					>
-						For you
-						{feedType === "forYou" && (
-							<div className='absolute bottom-0 w-10  h-1 rounded-full bg-primary'></div>
-						)}
-					</div>
-					<div
-						className='flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative'
-						onClick={() => setFeedType("following")}
-					>
-						Following
-						{feedType === "following" && (
-							<div className='absolute bottom-0 w-10  h-1 rounded-full bg-primary'></div>
-						)}
-					</div>
-				</div>
+    return (
+        <>
+            <div className='flex-[4_4_0] mr-auto border-r border-gray-700 min-h-screen'> {/* Main container */}
+                {/* Header */}
+                <div className='flex w-full border-b border-gray-700'>
+                    <div
+                        className={
+                            "flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative"
+                        }
+                        onClick={() => setFeedType("forYou")}
+                    >
+                        For you
+                        {feedType === "forYou" && (
+                            <div className='absolute bottom-0 w-10  h-1 rounded-full bg-primary'></div>
+                        )}
+                    </div> {/* Feed type: For You */}
+                    
+                    <div
+                        className='flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative'
+                        onClick={() => setFeedType("following")}
+                    >
+                        Following
+                        {feedType === "following" && (
+                            <div className='absolute bottom-0 w-10  h-1 rounded-full bg-primary'></div>
+                        )}
+                    </div> {/* Feed type: Following */}
+                </div>
 
-				{/*  CREATE POST INPUT */}
-				<CreatePost />
+                {/* Create Post Input */}
+                <CreatePost />
 
-				{/* POSTS */}
-				<Posts feedType={feedType} />
-			</div>
-		</>
-	);
+                {/* Posts */}
+                <Posts feedType={feedType} />
+            </div>
+        </>
+    );
 };
-export default HomePage;
+export default HomePage; // Export HomePage component
