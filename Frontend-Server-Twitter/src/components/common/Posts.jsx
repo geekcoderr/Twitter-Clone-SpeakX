c80d6cf59ba9e16d78a1bbd597683d3844d6d64a
Nishant Maheshwari <mnishant222@gmail.com>
2024-06-10 19:43:06 +0530

    Implementation of Frontend Done with Templates

---
+++ Frontend-Server-Twitter/src/components/common/Posts.jsx
@@ -0,0 +1,73 @@
+import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
+import PostSkeleton from "../skeletons/PostSkeleton";
+import Post from "./Post";
+
+const Posts = ({ feedType, username, userId }) => {
+	const getPostEndpoint = () => {
+		switch (feedType) {
+			case "forYou":
+				return "/api/posts/all";
+			case "following":
+				return "/api/posts/following";
+			case "posts":
+				return `/api/posts/user/${username}`;
+			case "likes":
+				return `/api/posts/likes/${userId}`;
+			default:
+				return "/api/posts/all";
+		}
+	};
+
+	const POST_ENDPOINT = getPostEndpoint();
+
+	const {
+		data: posts,
+		isLoading,
+		refetch,
+		isRefetching,
+	} = useQuery({
+		queryKey: ["posts"],
+		queryFn: async () => {
+			try {
+				const res = await fetch(POST_ENDPOINT);
+				const data = await res.json();
+
+				if (!res.ok) {
+					throw new Error(data.error || "Something went wrong");
+				}
+
+				return data;
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+	});
+
+	useEffect(() => {
+		refetch();
+	}, [feedType, refetch, username]);
+
+	return (
+		<>
+			{(isLoading || isRefetching) && (
+				<div className='flex flex-col justify-center'>
+					<PostSkeleton />
+					<PostSkeleton />
+					<PostSkeleton />
+				</div>
+			)}
+			{!isLoading && !isRefetching && posts?.length === 0 && (
+				<p className='text-center my-4'> <span style={{fontSize:'20px',color:'cyan'}}>  𝕏</span> You haven't Posted Yet!, Go a Head! </p>
+			)}
+			{!isLoading && !isRefetching && posts && (
+				<div>
+					{posts.map((post) => (
+						<Post key={post._id} post={post} />
+					))}
+				</div>
+			)}
+		</>
+	);
+};
+export default Posts;
---
+++ Frontend-Server-Twitter/vite.config.js
@@ -0,0 +1,16 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react()],
+	server: {
+		port: 3000,
+		proxy: {
+			"/api": {
+				target: "http://localhost:5000",
+				changeOrigin: true,
+			},
+		},
+	},
+});
